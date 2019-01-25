41edbdbb5fb391cae6376fea2c110473b7665387
Deesus <d.reddy@yandex.com>
2019-01-25 18:13:31 -0500

    Refactor: Remove redundant/temp code.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,13 +1,8 @@
 import Vue from 'vue';
-import Vuetify, { VLayout, VContainer, VFlex, VImg }  from 'vuetify/lib';
+import Vuetify from 'vuetify/lib';
 import 'vuetify/src/stylus/app.styl';
+
 
 Vue.use(Vuetify, {
     iconfont: 'md',
-    components: {
-        VLayout,
-        VContainer,
-        VFlex,
-        VImg
-    }
 });
--- tests/ListView.spec.js
+++ tests/ListView.spec.js
@@ -1,16 +1,16 @@
 import {shallowMount} from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
-
+import ListView from '@/views/ListView.vue';
 // we need to import vue and vuetify to prevent console warnings <https://github.com/vuetifyjs/vuetify/issues/243#issuecomment-288467099>:
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 Vue.use(Vuetify);
 
 
-describe('HelloWorld.vue', () => {
+
+describe('ListView.vue', () => {
     it('renders props.msg when passed', () => {
         const msg = 'new message';
-        const wrapper = shallowMount(HelloWorld, {
+        const wrapper = shallowMount(ListView, {
             propsData: {msg}
         });
         expect(wrapper.text()).not.toBeUndefined()
